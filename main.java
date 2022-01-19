84fd3a4bfb5cfb795bf7f20b26568597c65206be
sebastianf232 <87022337+sebastianf232@users.noreply.github.com>
2022-01-18 23:32:29 -0600

    Creación clase main e interfaz

---
+++ main.java
@@ -0,0 +1,9 @@
+/**
+ * Algoritmos y Estructura de Datos
+ * Universidad del Valle de Guatemala
+ * Hoja de Trabajo No.1
+ * @author Sebastián Franco - 21484
+ * @author Nicolle Escobar - 
+ * @author Luis Montenegro - 21699
+ * Clase main
+ */
