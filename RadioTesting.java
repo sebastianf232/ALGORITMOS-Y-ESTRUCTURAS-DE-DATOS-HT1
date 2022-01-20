d243f3eeb0d6a1c659971fc97fb87dae363b3385
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-01-20 01:39:17 -0600

    Add files via upload

--- RadioTesting.java
+++ RadioTesting.java
@@ -5,11 +5,26 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTesting {
 	RadioG9 radio = new RadioG9();
+
 	@Test
+	/**
+	 * Comprobar en qué estación
+	 * se encuentra el radio 
+	 */
 	void nextStation() {
 		RadioTesting test = new RadioTesting();
 		assertEquals(radio.getStation(),530);
 	}
+	
+	@Test
+	/**
+	 * Comprobar en qué frecuencia
+	 * se encuentra el radio
+	 */
+	void nextFrequency() {
+		assertEquals(radio.getFrequency(),true);
+	}
+	
 	
 	
 
